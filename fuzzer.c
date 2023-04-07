f169915978341291fc3355f23a5ec209ef6f3fad
miidzkow <michal.idzkowski@protonmail.com>
2023-04-07 13:07:54 +0200

    Added working extractor

--- fuzzer.c
+++ fuzzer.c
@@ -85,6 +85,41 @@ void write_tar_file(const char* filename, struct tar_t* header) {
     fclose(file);
 }
 
+int extract(char* extractor)
+{
+    int rv = 0;
+    char cmd[51];
+    strncpy(cmd, extractor, 25);
+    cmd[26] = '\0';
+    strncat(cmd, " testarchive.tar", 25);
+    char buf[33];
+    FILE *fp;
+
+    if ((fp = popen(cmd, "r")) == NULL) {
+        printf("Error opening pipe!\n");
+        return -1;
+    }
+
+    if(fgets(buf, 33, fp) == NULL) {
+        printf("No output\n");
+        goto finally;
+    }
+    if(strncmp(buf, "*** The program has crashed ***\n", 33)) {
+        printf("Not the crash message\n");
+        goto finally;
+    } else {
+        printf("Crash message\n");
+        rv = 1;
+        goto finally;
+    }
+    finally:
+    if(pclose(fp) == -1) {
+        printf("Command not found\n");
+        rv = -1;
+    }
+    return rv;
+}
+
 int main(int argc, char* argv[]){
 
     struct tar_t header;
@@ -99,6 +134,8 @@ int main(int argc, char* argv[]){
     // Write tar archive to file
     write_tar_file("testarchive.tar", &header);
 
+
+    extract(argv[1]);
 
     return 0;
 };
